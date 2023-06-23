c856d79c919125be5016298e40dbbb5be848a07d
w3villa-harsh-kumar <harsh.kumar@w3villa.com>
2023-06-23 05:59:33 +0530

    Update the code and Add comments

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const fs = require('fs').promises;
+const fs = require("fs").promises;
 
-const readFile = async () => {
+// read file
+const readFile = async (filePath) => {
     try {
-        const data = await fs.readFile(('learning/fileToRead.txt'), 'utf-8');
+        const data = await fs.readFile(filePath, "utf-8");
         return data;
     } catch (err) {
         console.log(err);
     }
-}
+};
 
-const writeFile = async (data) => {
+// write file
+const writeFile = async (filePath, ...data) => {
     try {
-        await fs.writeFile(('learning/fileToWrite.txt'), data);
+        await fs.writeFile(filePath, data.join("\n"), "utf-8");
     } catch (err) {
         console.log(err);
     }
-}
+};
 
+// main function
 const main = async () => {
-    const data = await readFile();
-    await writeFile(data);
-}
-
-
+    const data = await readFile("fileToRead1.txt");
+    const data2 = await readFile("fileToRead2.txt");
+    const data3 = await readFile("fileToRead3.txt");
+    await writeFile("fileToWrite.txt", data, data2, data3);
+};
 
 main();
